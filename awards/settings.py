f6ca00d65f8c9e4b5f38829e386bdc54cd842d14
kahenya-anita <anitakahenya1@gmail.com>
2020-10-23 13:58:23 +0300

    Add urls for login and registration

--- awards/settings.py
+++ awards/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = "/accounts/login/"
+#LOGOUT_REDIRECT_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
--- awards/urls.py
+++ awards/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('awardsapp.urls')),
+    url('^accounts/register/',
+        RegistrationView.as_view(success_url='/email'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
