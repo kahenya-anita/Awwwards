a03db1652051475411e4e49cb965840cdcd89cc1
kahenya-anita <anitakahenya1@gmail.com>
2020-10-25 02:41:17 +0300

    Add migrations

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('/',views.home,name = 'home'),
+    path('',views.home,name = 'home'),
     path('email/',views.email,name = 'email'),
     path('create_profile/',views.create_profile,name = 'create_profile'),
-    path('profile/(?P<profile_id>\d+)',views.profile,name = 'profile'),
-    path('project/(?P<project_id>\d+)',views.project,name = 'project'),
+    path('profile/(<profile_id>\)',views.profile,name = 'profile'),
+    path('project/(<project_id>\)',views.project,name = 'project'),
     path('add_project/',views.add_project,name = 'add_project'),
-    path('rate_project/(?P<project_id>\d+)',views.rate_project,name = 'rate_project'),
+    path('rate_project/(<project_id>\)',views.rate_project,name = 'rate_project'),
     path('search_project/',views.search_project,name = 'search_project'),
-    path('api/projects/', views.ProjectList.as_view()),
-    path('api/profiles/', views.ProfileList.as_view()),
+    
 ]
