cadeebfcea1a9c6933ab966a7cb33339e20df006
kahenya-anita <anitakahenya1@gmail.com>
2020-10-25 02:16:36 +0300

    Added views urls

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -6,4 +6,10 @@ urlpatterns=[
     path('email/',views.email,name = 'email'),
     path('create_profile/',views.create_profile,name = 'create_profile'),
     path('profile/(?P<profile_id>\d+)',views.profile,name = 'profile'),
+    path('project/(?P<project_id>\d+)',views.project,name = 'project'),
+    path('add_project/',views.add_project,name = 'add_project'),
+    path('rate_project/(?P<project_id>\d+)',views.rate_project,name = 'rate_project'),
+    path('search_project/',views.search_project,name = 'search_project'),
+    path('api/projects/', views.ProjectList.as_view()),
+    path('api/profiles/', views.ProfileList.as_view()),
 ]
