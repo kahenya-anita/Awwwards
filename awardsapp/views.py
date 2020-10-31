9555fdf18e139497e7c0ff39c87d445c1bc7d4f6
kahenya-anita <anitakahenya1@gmail.com>
2020-11-01 00:29:52 +0300

    Afew changes

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -74,7 +74,7 @@ def email(request):
     send_signup_email(name, email)
     return redirect(create_profile)
 
-@login_required
+@login_required(login_url='registration/login/')
 def home(request):
     title= "aWWWards"
     date = dt.date.today()
